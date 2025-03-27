5188c802ee30ecb3e54126f4c696f1f7047681f8
cruzhit0 <carlos.cruz@ucb.edu.bo>
2025-03-26 22:52:40 -0400

    dejar abierto el punto de crear usuario para registro

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -4,9 +4,11 @@ import { authMiddleware } from '@core/middleware/authMiddleware';
 
 const router = Router();
 const userController = new UserController();
-router.use(authMiddleware);
 
 router.post('/', userController.createUser);
+
+//rutas protegidas
+router.use(authMiddleware);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
